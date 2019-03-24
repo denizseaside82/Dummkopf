b8f33f69e1fc77007ba68e64093c667597b237fb
DS <deniz.karadal@gmail.com>
2019-03-24 15:14:34 +0100

    First commit after initialising a part of the structure

---
+++ src/Game.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    private List<Player> players = new ArrayList<>();
+
+    public Game (List<Player> players) {
+        this.players = players;
+    }
+
+    public void addPlayer(Player player){
+        players.add(player);
+    }
+
+    public Player getPlayer(int index){
+        return players.get(index);
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,21 @@
+public class Player {
+    private String name;
+    private int points;
+
+    public Player (String name, int points) {
+        this.name = name;
+        this.points = points;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public int getPoints () {
+        return points;
+    }
+
+    public void setPoints (int points) {
+        this.points = points;
+    }
+}
