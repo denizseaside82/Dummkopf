6a98a1175aa02eec9b8821d2d94a7a0b4913be25
denizseaside82 <deniz.karadal@gmail.com>
2019-03-26 23:20:16 +0100

    Still trying to set logically the structure in a tiny step

--- src/Card.java
+++ src/Card.java
@@ -1,14 +1,12 @@
 public class Card {
     private String suit, rank;
-    private static final Random GENERATOR = new Random();
-
     private static final String[] SUITS = {"clubs", "diamonds", "hearts", "spades"};
     private static final String[] RANKS = {"Ace", "Jack", "Queen", "King", "10", "9", "8", "7", "6"};
 
-    //Randomly set SUITS and Ranks card
+//    Randomly set SUITS and Ranks card
     public Card() {
-        suit = SUITS[GENERATOR.nextInt(SUITS.length)];
-        rank = RANKS[GENERATOR.nextInt(RANKS.length)];
+//        suit = SUITS[GENERATOR.nextInt(SUITS.length)];
+//        rank = RANKS[GENERATOR.nextInt(RANKS.length)];
     }
 
     public String getSuit() {
---
+++ src/Deck.java
@@ -0,0 +1,4 @@
+public class Deck {
+    private static final Random GENERATOR = new Random();
+    private ArrayList<Card> cards = new ArrayList<Card>();
+}
