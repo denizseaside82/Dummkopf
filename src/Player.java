9a9a79497271f2db1f76e18cd945537594b112ff
DS <deniz.karadal@gmail.com>
2019-07-20 00:08:58 +0200

    Simpfly the logic and refactor

--- src/Player.java
+++ src/Player.java
@@ -24,14 +24,47 @@ public class Player {
         }
     }
 
-    public void getSuitInHand() {
+    public int getSuitInHand() {
+        int lowestCard = 13;
+        int foundCard = 0;
         int i = 0;
             while (i < 6) {
                 if(Dealer.getTrumpFromDeck().getSuit() == this.hand.get(i).getSuit()){
-                    System.out.println("Player : " + this.getName() + " has a same suit at row " + (i+1));
+//                    System.out.println("Player : " + this.getName() + " has a same suit at row " + (i+1));
+                    int actualCard = this.hand.get(i).getRank().getValue();
+
+                    switch (actualCard){
+                        case 1:
+                           lowestCard = 1;
+                        case 6:
+                            lowestCard = 6;
+                        case 7:
+                            lowestCard = 7;
+                        case 8:
+                            lowestCard = 8;
+                        case 9:
+                            lowestCard = 9;
+                        case 10:
+                            lowestCard = 10;
+                        case 11:
+                            lowestCard = 11;
+                        case 12:
+                            lowestCard = 12;
+                        case 13:
+                            lowestCard = 13;
+                    }
+
+//                    foundCard = actualCard <= lowestCard ? actualCard : lowestCard;
+                    if (actualCard <= lowestCard ){
+                        foundCard = actualCard;
+                    }
+
+//                    System.out.println("Player : " + this.getName() + " " + foundCard);
                 }
                 i++;
             }
+
+            return foundCard;
     }
 }
 
