752a6e1eec33ca7a2a47f7a55fde18a4b587ec61
DS <deniz.karadal@gmail.com>
2019-08-03 22:55:51 +0200

    Simpfly the logic and refactor

--- src/Player.java
+++ src/Player.java
@@ -25,40 +25,31 @@ public class Player {
 
     public int getSuitInHand() {
         int lowestCard = 14;
-        int i = 0;
         int actualCard;
         List<Integer> countTrump = new ArrayList<>();
+
+        int i = 0;
         while (i < 6) {
             if (Dealer.getTrumpFromDeck().getSuit() == this.hand.get(i).getSuit()) {
-                actualCard = this.hand.get(i).getRank().getValue();
-                countTrump.add(actualCard);
-
-                if (actualCard < lowestCard) {
-                    if(countTrump.size() > 1){
-                        for (int index : countTrump){
-                            if(index == 1){
-                                System.out.println("Gotcha");
-                                break;
-                            }
-                        }
-                    }
-//                    if(countTrump.contains(1) == true){
-//                        System.out.println("It contains ACE card as alone");
-//                    }
-                    lowestCard = actualCard;
-                }
-                System.out.println("Player : " + this.getName() + " has " + this.hand.get(i));
-//                System.out.println("Array size" + countTrump.size());
-
-            }else{
-                if(i == 5 && lowestCard == 14) {
-                    System.out.println("Player " + this.getName() + " has no TRUMP");
-                }
-//                }else if(i == 5 && this.hand.size() == 1 && this.hand.get(i).getRank() == Card.Rank.ACE ){
-//                    System.out.println("");
-//                }
+                countTrump.add(this.hand.get(i).getRank().getValue());
             }
             i++;
+        }
+
+        if (countTrump.size() >! 1 && countTrump.get(0) == 1) {
+            countTrump.remove(0);
+        }
+
+        Iterator countIterator = countTrump.iterator();
+        while (countIterator.hasNext()) {
+            actualCard = (int) countIterator.next();
+
+            if (actualCard < lowestCard) {
+                lowestCard = actualCard;
+            } else if (i == 5 && lowestCard == 14) {
+                System.out.println("Player " + this.getName() + " has no TRUMP");
+            }
+            System.out.println("Player : " + this.getName() + " has " + actualCard);
         }
 
         return lowestCard;
